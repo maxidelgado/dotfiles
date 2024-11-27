de995631d518e6de3fb5f26c472cee8c955b7931
Maximiliano Delgado <maxidelga2@gmail.com>
2024-11-27 08:39:50 -0300

    fix

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 stow .
-ln -sf ./nvim/.nvim_config.lua ~/.nvim_config.lua
-ln -sf ./zshrc/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/nvim/.nvim_config.lua ~/.nvim_config.lua
+ln -sf ~/dotfiles/zshrc/.zshrc ~/.zshrc
