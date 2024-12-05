498808865e1ea724c1dca8ebfb29e77f2dc447d6
Maximiliano Delgado <maxidelga2@gmail.com>
2024-12-05 15:17:29 -0300

    add 1pwd setup

--- setup.sh
+++ setup.sh
@@ -3,3 +3,5 @@ xargs brew install < ./homebrew/leaves.txt
 stow .
 ln -sf ~/dotfiles/nvim/.nvim_config.lua ~/.nvim_config.lua
 ln -sf ~/dotfiles/zshrc/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.ssh/config ~/.ssh/config
+mkdir -p ~/.1password && ln -s ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock ~/.1password/agent.sock
