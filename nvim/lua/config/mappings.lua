c004f7f081869cb1ac2488e78918d52c11355086
Maximiliano Delgado <maxidelga2@gmail.com>
2024-12-05 12:59:15 -0300

    move lines

--- nvim/lua/config/mappings.lua
+++ nvim/lua/config/mappings.lua
@@ -87,3 +87,9 @@ map("n", "<leader>za", "zg", { desc = "Add word" })
 -- Reload snippets folder
 -- TODO make path system independent
 map("n", "<leader>ms", "<cmd>source ~/.config/nvim/snippets/*<cr>", { desc = "Reload snippets" })
+
+-- Move line
+map("n", "<A-j>", ":m .+1<CR>==") -- move line up(n)
+map("n", "<A-k>", ":m .-2<CR>==") -- move line down(n)
+map("v", "<A-j>", ":m '>+1<CR>gv=gv") -- move line up(v)
+map("v", "<A-k>", ":m '<-2<CR>gv=gv") -- move line down(v)
