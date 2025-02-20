f734d669b5dc04a1cc9105e68c598304a89ae07c
Maximiliano Delgado <mdelgado@MacbookPro.local>
2025-02-20 19:01:51 -0300

    add nvim submodule to facilitate code maintainance

--- nvim/init.lua
+++
@@ -1,4 +0,0 @@
--- References to ./lua/
--- HACK: to prevent deprecation spam at start
--- vim.deprecate = function() end ---@diagnostic disable-line: duplicate-set-field
-require("config")
--- nvim/lua/core/plugins/cql.lua
+++
@@ -1,3 +0,0 @@
-return {
-  'elubow/cql-vim',
-}
